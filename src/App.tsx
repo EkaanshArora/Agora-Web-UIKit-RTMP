fa84ba9537c850375ebb36623afad50b43b18aa3
EkaanshArora <ekaansh68@gmail.com>
2022-01-12 18:57:01 +0530

    add RTMP stream to UIKit demo

---
+++ src/App.tsx
@@ -0,0 +1,78 @@
+import React, { useContext, useState } from 'react'
+import { PropsContext, GridVideo, layout, LocalControls, PinnedVideo, RtcConfigure, TracksConfigure } from 'agora-react-uikit'
+import 'agora-react-uikit/dist/index.css'
+import RTMP from './RTMP'
+
+const App: React.FunctionComponent = () => {
+  const [videocall, setVideocall] = useState(true)
+  const [isHost, setHost] = useState(true)
+  const [isPinned, setPinned] = useState(false)
+
+  return (
+    <div style={styles.container}>
+      <div style={styles.videoContainer}>
+        <h1 style={styles.heading}>Agora React Web UI Kit</h1>
+        {videocall ? (<>
+          <div style={styles.nav}>
+            <p style={{ fontSize: 20, width: 200 }}>You're {isHost ? 'a host' : 'an audience'}</p>
+            <p style={styles.btn} onClick={() => setHost(!isHost)}>Change Role</p>
+            <p style={styles.btn} onClick={() => setPinned(!isPinned)}>Change Layout</p>
+          </div>
+          <PropsContext.Provider value={{
+            rtcProps: {
+              appId: '<Your Agora App ID>',
+              channel: 'test',
+              token: null,
+              role: isHost ? 'host' : 'audience',
+              layout: isPinned ? layout.pin : layout.grid
+            },
+            styleProps: {
+              gridVideoContainer: { 'height': '90%' },
+              pinnedVideoContainer: { 'height': '90%' }
+            },
+            callbacks: {
+              EndCall: () => setVideocall(false),
+            }
+          }} >
+
+            <div style={{ flex: 1 }} >
+              {isHost === false ? (
+                <VideocallUI />
+              ) : (
+                <TracksConfigure>
+                  <VideocallUI />
+                </TracksConfigure>
+              )}
+            </div>
+          </PropsContext.Provider>
+        </>
+        ) : (
+          <div style={styles.nav}>
+            <h3 style={styles.btn} onClick={() => setVideocall(true)}>Start Call</h3>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+const styles = {
+  container: { width: '100vw', height: '100vh', display: 'flex', flex: 1, backgroundColor: '#007bff22' },
+  heading: { textAlign: 'center' as const, marginBottom: 0 },
+  videoContainer: { display: 'flex', flexDirection: 'column', flex: 1 } as React.CSSProperties,
+  nav: { display: 'flex', justifyContent: 'space-around' },
+  btn: { backgroundColor: '#007bff', cursor: 'pointer', borderRadius: 5, padding: 5, color: '#ffffff', fontSize: 20 },
+}
+
+const VideocallUI = () => {
+  const { rtcProps } = useContext(PropsContext)
+  return (
+    <RtcConfigure callActive={rtcProps.callActive}>
+      <RTMP />
+      {rtcProps?.layout === layout.grid ? <GridVideo /> : <PinnedVideo />}
+      <LocalControls />
+    </RtcConfigure>
+  )
+}
+
+export default App
---
+++ src/RTMP.tsx
@@ -0,0 +1,110 @@
+import { MaxUidContext, MinUidContext, RtcContext } from 'agora-react-uikit';
+import { useContext, useEffect, useState } from 'react';
+
+const RTMPUrl =
+  '';
+const W = 1280
+const H = 720
+
+const RTMP = () => {
+  const [isStreaming, setStreaming] = useState(false);
+  const [isBtnDisabled, setBtnDisabled] = useState(false)
+  const { client, localUid } = useContext(RtcContext)
+  const max = useContext(MaxUidContext);
+  const min = useContext(MinUidContext);
+
+  useEffect(() => {
+    client.on('live-streaming-error', (e) =>
+      console.log('live-streaming-error', e),
+    );
+    client.on('live-streaming-warning', (e) =>
+      console.log('live-streaming-warning', e),
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (isStreaming) {
+      transcode();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isStreaming, max, min]);
+
+  const transcode = async () => {
+    let users = [...max, ...min];
+    let N = users.length;
+    let rows = Math.round(Math.sqrt(N));
+    let cols = Math.ceil(N / rows);
+    let uids = users.map((u) => {
+      if (u.uid === 0 && localUid) {
+        return localUid as number;
+      } else {
+        return u.uid as number;
+      }
+    });
+    let userData = uids.map((u, i) => {
+      return {
+        uid: u,
+        x: (i % cols) * Math.round(W / cols),
+        y: Math.round(Math.floor(i / cols) * (H / rows)),
+        width: Math.round(W / cols),
+        height: Math.round(H / rows),
+        zOrder: 0,
+        alpha: 1,
+      };
+    });
+    const config = {
+      // Width of the video (px). The default value is 640.
+      width: W,
+      // Height of the video (px). The default value is 360.
+      height: H,
+      // Frame rate of the video (fps). The default value is 15.
+      videoFramerate: 30,
+      backgroundColor: 0x000000,
+      watermark: {
+        url: '',
+        x: 10,
+        y: 10,
+        width: 200,
+        height: 70,
+      },
+      // Set the layout for each user.
+      transcodingUsers: userData,
+    };
+    await client.setLiveTranscoding(config)
+  };
+
+  const rtmpOn = async () => {
+    setBtnDisabled(true)
+    await transcode()
+    await client.startLiveStreaming(RTMPUrl, true)
+    setStreaming(true)
+    setBtnDisabled(false)
+  }
+
+  const rtmpOff = async () => {
+    setBtnDisabled(true)
+    await client.stopLiveStreaming(RTMPUrl)
+    setStreaming(false)
+    setBtnDisabled(false)
+  }
+
+  return (
+    <div style={container}>
+      <div style={isBtnDisabled ? disabledBtn : btn} onClick={() => {
+        if (!isBtnDisabled) {
+          console.log(isStreaming)
+          isStreaming ? rtmpOff() : rtmpOn()
+        }
+      }}>
+        {isBtnDisabled ? 'Working...' : isStreaming ? 'Stop Streaming' : 'Start Streaming'}
+      </div>
+    </div>
+  )
+}
+
+const container = {display: 'flex', flex: 1, alignContent: 'center', alignItems: 'center', marginBottom: 10}
+const btn = { backgroundColor: '#007bff', cursor: 'pointer', borderRadius: 5, padding: 5, color: '#ffffff', fontSize: 20, margin: 'auto',  paddingLeft: 10, paddingRight: 10}
+const disabledBtn = { backgroundColor: '#007bff55', borderRadius: 5, padding: 5, color: '#ffffff', fontSize: 20, margin: 'auto', paddingLeft: 20, paddingRight: 20}
+
+export default RTMP;
